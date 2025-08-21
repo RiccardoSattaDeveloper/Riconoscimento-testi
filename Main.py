2a2deb677f1cc23aa25e6b2acc9f6ba68873d738
Riccardo Satta <109417824+RiccardoSattaDeveloper@users.noreply.github.com>
2025-08-21 14:45:56 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
-from GestoreDocumenti import GestoreDocumenti # Importa la classe per gestire i documenti.
-from InterfacciaRicerca import InterfacciaRicerca # Importa la classe per l’interfaccia grafica di ricerca.
+from GestoreDocumenti import GestoreDocumenti 
+from InterfacciaRicerca import InterfacciaRicerca 
 
-percorso_documenti = "/home/ricky/Scrivania/Riconoscimento testi/Testi di Fabrio De Andrè" # Percorso della cartella con i documenti.
+percorso_documenti = "/home/ricky/Scrivania/Riconoscimento testi/Testi di Fabrio De Andrè" 
 
-gestore = GestoreDocumenti(percorso_documenti) # Crea un’istanza del gestore dei documenti caricando i file.
+gestore = GestoreDocumenti(percorso_documenti) 
 
-interfaccia = InterfacciaRicerca(gestore) # Crea l’interfaccia grafica passando il gestore dei documenti.
+interfaccia = InterfacciaRicerca(gestore) 
 
-interfaccia.avvia() # Avvia l’interfaccia grafica, mostrando la finestra all’utente.
+interfaccia.avvia()
